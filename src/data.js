1df8fc904e48a7667470f8105970f61b151fc304
Vineeth <vntnambiar4@gmail.com>
2021-10-19 20:09:16 +0530

    optimisations

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Tari App",
+    subtitle: "React Native",
+    description:
+      "Tari App is an all-in-one nutrition, fitness, and health tracking app that focuses on accountability between friends and coaches.PlayStore Link",
+    image: "./project-1.gif",
+    link: "https://play.google.com/store/apps/details?id=com.meditari.tariapp",
+  },
+  {
+    title: "NextPaw",
+    subtitle: "React Native",
+    description:
+      "NextPaw Helps Pet Parents Find and Choose Your Local Business.",
+    image: "./project-2.gif",
+    link: "https://play.google.com/store/apps/details?id=com.nextpaw.dashboard",
+  },
+  {
+    title: " OYA app",
+    subtitle: "React Native",
+    description:
+      "The OYA app empowers users to find woman-owned businesses in their area, and connect with them through visual content and consumer reviews",
+    image: "./project-3.gif",
+    link: "https://play.google.com/store/apps/details?id=com.oyacard.oya&hl=en",
+  },
+  {
+    title: "Ethora",
+    subtitle: "React Native, XMPP Protocol",
+    description:
+      "Mobile app engine and platform for social tokenized communities.",
+    image: "./project-4.gif",
+    link: "https://ethora.com/wiki/Main_Page",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
